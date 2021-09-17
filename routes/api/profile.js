dbf9c2a342f2f8597f8021796281642fa32b3ab9
Katrin Sukovati <katrin.sukovati1@gmail.com>
2021-09-17 14:11:47 -0400

    Created the profile model and got the current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,30 @@
-// has routes that have anything to do with profiles, fetching them, adding them, updating them etc
+// Has routes that have anything to do with profiles, fetching them, adding them, updating them etc
 
-const express = require("express");
+const express = require('express');
 router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-// @route  GET api/profile
-// @desc   Test route
-// @access Public
-router.get('/',(req,res)=> res.send('Profile route'));
+// @route  GET api/profile/me
+// @desc   Get current users profile
+// @access Private --> becuase were getting the profile by the usre id thats inside the token (bring in the auth middleware)
+// add auth as a second parameter to routes that we want to protect
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name,avatar']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.err(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;

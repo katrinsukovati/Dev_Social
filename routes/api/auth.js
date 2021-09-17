d170273a1e22b1c0d0e5e0e10e7e8a294cbfa2ed
Katrin Sukovati <katrin.sukovati1@gmail.com>
2021-09-17 13:42:23 -0400

    Developed the user authentication and login route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,6 +6,14 @@ router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
 
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+// express-validator is a set of express.js middlewares that wraps validator.js validator and sanitizer functions.
+// It will validate the users input
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
 // @route  GET api/auth
 // @desc   Test route
 // @access Public
@@ -21,5 +29,72 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route  POST api/auth
+// @desc   Authenticate user & get token
+// @access Public
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    // Handle the response
+    const errors = validationResult(req);
+    // If there are errors
+    if (!errors.isEmpty()) {
+      // 400 is a bad request
+      //.json makes the errors visible
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Destructuring:
+    const { email, password } = req.body;
+
+    try {
+      // See if the user exists, if they dont exist, send an error
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      // Make sure the passwords matche
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      // If theres no match
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      // Return the json web token (authorization) --> to allow them to log in right away
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      // Sign the token
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        // Before you deploy, change it to 3600
+        { expiresIn: 3600000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      // Server error
+      console.error(err);
+      res.send(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;

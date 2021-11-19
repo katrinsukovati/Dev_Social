8a0c10a68023d2b5feab82c702001ccbfe7cbd8c
Katrin Sukovati <katrin.sukovati1@gmail.com>
2021-11-19 15:06:31 -0500

    ProfileTop and ProfileAbout Components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import { Link } from 'react-router-dom';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 
 const Profile = ({
   getProfileById,
@@ -33,6 +35,11 @@ const Profile = ({
                 Edit Profile{' '}
               </Link>
             )}
+          <div class='profile-grid my-1'>
+            <ProfileTop profile={profile}/>
+            <ProfileAbout profile={profile}/>
+            
+          </div>
         </Fragment>
       )}
     </Fragment>
